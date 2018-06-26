fb34e51ab031ffe96bfeadba49473d8fbda25e7e
ginyn <zejin666@qq.com>
2018-06-27 01:15:20 +0800

    test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='ssdata',
+      version='0.1',
+      description='test',
+      url='http://github.com/tongling/clinicaltrial',
+      author='zejin',
+      author_email='ginynu@gmail.com',
+      license='MIT',
+      packages=['ssdata',],
+      zip_safe=False)
